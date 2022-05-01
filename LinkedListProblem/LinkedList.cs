7d4248280d99e9e9cbab7274224549ba74bf5e35
59114_Chetana <deslechetna1999@gmail.com>
2022-05-02 00:00:07 +0530

    [Chetana]Added:add elements in reverse order in Linked List

--- LinkedListProblem/LinkedList.cs
+++ LinkedListProblem/LinkedList.cs
@@ -27,6 +27,20 @@ namespace LinkedListProblem
                 Console.WriteLine("inserted into Linked List " + node.data);
             }
         }
+        public void AddInReverseOrder(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                head = newNode;
+                head.next = temp;
+            }
+        }
         public void Display()
         {
             Node temp = this.head;
--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -4,11 +4,35 @@ public class Program
 {
     public static void Main(String[] args)
     {
+        Console.WriteLine("WELCOME IN THE LINKEDLIST");
         LinkedList list = new LinkedList();
-        list.Add(56);
-        list.Add(30);
-        list.Add(70);
-        list.Display();
+        bool check = true;
+        Console.WriteLine(" 1 add \n 2.Add Reverse \n 3. Display \n 4.end");
+        while (check)
+        {
+            Console.WriteLine(" **** ENTER THE ABOVE OPTION TO EXECUTE THE METHODS *****");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Console.WriteLine("LINKED LIST");
+                    list.Add(56);
+                    list.Add(30);
+                    list.Add(70);
+                    break;
+                 case 2:
+                    list.AddInReverseOrder(70);
+                    list.AddInReverseOrder(30);
+                    list.AddInReverseOrder(56);
+                    break;
+                case 3:
+                    list.Display();
+                    break;
+                case 4:
+                    check = false;
+                    break;
+            }
+        }
 
     }
 }
